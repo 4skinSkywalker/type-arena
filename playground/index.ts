7325585c6fa3e98627127a734b69f2e9260ceb15
Fred's GitHub <stopchemtrailsfred@gmail.com>
2025-09-01 01:08:38 +0200

    fix bob dylan quote

--- playground/index.ts
+++ playground/index.ts
@@ -1,6 +1,14 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
+    const args = process.argv.slice(2);
+    const roomId = args[0];
+
+    if (!roomId) {
+        console.error('Please provide a roomId as an argument.');
+        process.exit(1);
+    }
+
     const windowWidth = 1400;
     const windowHeight = 800;
     const windowXPos = 0;
@@ -23,23 +31,48 @@ const puppeteer = require('puppeteer');
     });
 
     await page.evaluateOnNewDocument(() => {
-        localStorage.setItem('clientInfo', JSON.stringify({ name: "Fredo", car: 1, wpm: 0, accuracy: 0 }));
+        const nicks = [
+            "Shadow",
+            "Hunter",
+            "Starlight",
+            "Archer",
+            "Wolf666",
+            "Owlbe",
+            "Maverick",
+            "Vagabond",
+            "WildCard",
+            "Frostbite",
+            "Javaman",
+            "DonaldTrump"
+        ];
+        localStorage.setItem('clientInfo', JSON.stringify({ name: nicks[Math.floor(Math.random()*nicks.length)], car: 1+Math.floor(Math.random()*7), wpm: 0, accuracy: 0 }));
     });
 
-    await page.goto('https://type-arena.dev/multiplayer/mx9ncoqbqw9', { waitUntil: 'networkidle0' });
+    await page.goto('https://type-arena.dev/multiplayer/' + roomId, { waitUntil: 'networkidle0' });
 
     await new Promise(resolve => setTimeout(resolve, 2000));
 
     await page.evaluate(() => {
         eval(`
 (function() {
-    /*
-    TODO:
-    1. Various level of skills
-    2. Make it know when it's the admin
-    3. Make it wait 15s or start if everybody has finished
-    */
-    const avgTypingSpeed = 170;
+    const difficulties = {
+        easy: {
+            avgTypingSpeed: 200,
+            errRate: 0.1
+        },
+        medium: {
+            avgTypingSpeed: 170,
+            errRate: 0.05
+        },
+        hard: {
+            avgTypingSpeed: 140,
+            errRate: 0.01
+        }
+    };
+    const _difficulties = Object.values(difficulties);
+    const difficulty = _difficulties[Math.floor(Math.random()*_difficulties.length)];
+    const {avgTypingSpeed, errRate} = difficulty;
+
     let raceStarted = false;
     let quote = "";
     let textIndex = 0;
@@ -60,6 +93,8 @@ const puppeteer = require('puppeteer');
         );
     }
     
+    let hasError = false;
+    let prevChar = "";
     function typingLoop() {
         const rnd = Math.random() * avgTypingSpeed;
         const signRnd = Math.random() > 0.5 ? rnd : -rnd;
@@ -68,8 +103,25 @@ const puppeteer = require('puppeteer');
                 return;
             }
             
-            const char = quote[textIndex++];
+            let char;
+            if (hasError) {
+                text = text.slice(0, textIndex);
+                char = prevChar;
+                hasError = false;
+                prevChar = "";
+                textIndex++;
+            } else {
+                if (Math.random() < errRate) {
+                    char = "#";
+                    hasError = true;
+                    prevChar = quote[textIndex];
+                } else {
+                    char = quote[textIndex];
+                    textIndex++;
+                }
+            }
             text += char;
+            
             type();
             
             if (text.length < quote.length) {
